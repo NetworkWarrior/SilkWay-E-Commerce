5d24335adbf2bf114278c7734ff086d154b78af2
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-12-13 00:49:14 +0100

    creating the singular product page

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . models import Category, Product
-
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 def store(request):
@@ -11,5 +11,12 @@ def store(request):
 def categories(request):
     all_categories = Category.objects.all()
     return {"all_categories": all_categories}
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {'product': product}
+    return render(request, 'store/product-info.html', context)
+
 
 
