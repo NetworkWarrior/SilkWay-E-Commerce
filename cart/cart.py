7e1dd2c0561eff4346a67edcd4bb162ed500b717
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-12-13 09:18:43 +0100

    update the cart summary template

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+from store.models import Product
+
 class Cart():
 
     def __init__(self, request):
@@ -16,6 +19,7 @@ class Cart():
 
         self.cart = cart
 
+
     def add(self, product, product_qty):
         product_id = str(product.id)
         if product_id in self.cart:
@@ -24,5 +28,23 @@ class Cart():
             self.cart[product_id] = {'price': str(product.price), 'qty': product_qty}
         self.session.modified = True
 
+
     def __len__(self):
         return sum(item['qty'] for item in self.cart.values())
+    
+
+    def __iter__(self):
+        all_product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=all_product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total'] = item['price'] * item['qty']
+            yield item
+
+        
