ff263a3d49f30830a070d20dc157d5bf5a6e3cdf
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-12-13 01:34:51 +0100

    buildingg the product category data grid

--- store/views.py
+++ store/views.py
@@ -15,7 +15,7 @@ def categories(request):
 def list_category(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
-    return render(request, 'list-category.html', {'category': category, 'products':products})
+    return render(request, 'store/list-category.html', {'category': category, 'products':products})
 
 
 def product_info(request, product_slug):
