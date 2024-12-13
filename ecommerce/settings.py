f1124ac1302a68192b1bec083924045edd3c7f25
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-12-13 01:48:19 +0100

    configure basic urls and views for our cart

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
+    'store', #django app
+    'cart', #django app
 ]
 
 MIDDLEWARE = [
