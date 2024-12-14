885cfe8cda47d8e66d3c545f05569227241a16bf
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-12-14 04:31:39 +0100

    configuring cart functionality delete and update

--- cart/cart.py
+++ cart/cart.py
@@ -29,6 +29,21 @@ class Cart():
         self.session.modified = True
 
 
+    def delete(self, product):
+        product_id = str(product)
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.session.modified = True
+
+
+    def update(self, product, qty):
+        product_id = str(product)
+        product_quantity = qty
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = product_quantity
+        self.session.modified = True
+        
+
     def __len__(self):
         return sum(item['qty'] for item in self.cart.values())
     
@@ -36,9 +51,8 @@ class Cart():
     def __iter__(self):
         all_product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=all_product_ids)
-
-        cart = self.cart.copy()
-
+        import copy
+        cart = copy.deepcopy(self.cart)
         for product in products:
             cart[str(product.id)]['product'] = product
 
