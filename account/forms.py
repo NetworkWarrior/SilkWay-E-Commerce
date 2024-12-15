e133402dcc7cccb8965480638dece2667a1221ad
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-12-15 18:09:12 +0100

    sything the email verification

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms.widgets import PasswordInput, TextInput
 
+#Registration Form
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
