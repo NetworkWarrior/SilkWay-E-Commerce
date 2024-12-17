05de62144a09b5555c4ff242541f7fa618613422
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2024-12-17 17:10:30 +0100

    process order functionality

--- payment/models.py
+++ payment/models.py
@@ -34,7 +34,7 @@ class ShippingAdress(models.Model):
 class Order(models.Model):
     full_name = models.CharField(max_length=300)
     email = models.EmailField(max_length=255)
-    shipping_adress = models.TextField(max_length=1000)
+    shipping_address = models.TextField(max_length=1000)
     amount_paid = models.DecimalField(max_digits=8, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
